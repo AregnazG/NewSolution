78f0a70b554ce46fcd5b6222bce3542a01faea7b
Aryush <gayranyan.aregnaaz@mail.ru>
2022-09-16 01:43:46 +0400

    Helix-Task

---
+++ HelixTas/app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function index()
+    {
+
+        return view('auth.login');
+
+    }
+
+    public function postLogin(Request $request)
+    {
+
+        $request->validate([
+
+            'username' => 'required|max:10',
+            'password' => 'required|min:6|max:8'
+
+        ]);
+
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $user = User::where('username', '=', $username)->first();
+
+        if($user->password == $password) {
+
+            Auth::login($user);
+
+            return redirect('profile');
+
+        }
+
+        return redirect('login')->with('succes', 'Login details are not valid');
+
+    }
+
+    public function profile()
+    {
+
+        return view('auth.profile');
+
+    }
+
+}
--- HelixTas/routes/web.php
+++ HelixTas/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthController::class)->group(function (){
+
+    Route::get('login', 'index')->name('login');
+    Route::get('profile', 'profile')->middleware('user.auth.check');
+    Route::post('post-login', 'postLogin')->name('login.post');
+
 });
